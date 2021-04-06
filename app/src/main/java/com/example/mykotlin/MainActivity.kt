deb4431ee902b502aa736baea09da8c4cfc26ad4
EomJunyoung <theoneday737@naver.com>
2021-04-07 08:33:53 +0900

    add activity and button listener

--- app/src/main/java/com/example/mykotlin/MainActivity.kt
+++ app/src/main/java/com/example/mykotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mykotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -19,7 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         // Set contactItemClick & contactItemLongClick lambda
         val adapter = ContactAdapter({ contact ->
-            // put extras of contact info & start AddActivity
+            val intent = Intent(this, AddActivity::class.java)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_NAME, contact.name)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_NUMBER, contact.number)
+            intent.putExtra(AddActivity.EXTRA_CONTACT_ID, contact.id)
+            startActivity(intent)
         }, { contact ->
             deleteDialog(contact)
         })
@@ -33,15 +38,20 @@ class MainActivity : AppCompatActivity() {
         contactViewModel.getAll().observe(this, Observer<List<Contact>> { contacts ->
             adapter.setContacts(contacts!!)
         })
+
+        main_button.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun deleteDialog(contact: Contact) {
         val builder = AlertDialog.Builder(this)
         builder.setMessage("Delete selected contact?")
-            .setNegativeButton("NO") { _, _ -> }
-            .setPositiveButton("YES") { _, _ ->
-                contactViewModel.delete(contact)
-            }
+                .setNegativeButton("NO") { _, _ -> }
+                .setPositiveButton("YES") { _, _ ->
+                    contactViewModel.delete(contact)
+                }
         builder.show()
     }
 
