6fb8b88c8785b5b9a42e5e4cd71938bedac85ce6
EomJunyoung <theoneday737@naver.com>
2021-04-09 12:56:49 +0900

    add mypagelayout

--- app/src/main/java/com/example/mykotlin/presentation/view/MypageActivity.kt
+++ app/src/main/java/com/example/mykotlin/presentation/view/MypageActivity.kt
@@ -13,12 +13,15 @@ class MypageActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_mypage)
 
+        //DataBinding을 이용하게되면, xml명을 기반으로 뒤에 +Binding명칭이 붙은 객체가 하나 만들어짐
+        //이를 통해 DataBinding된 xml을 inflate하고, id를 불러올 수 있다.
         var binding: ActivityMypageBinding = DataBindingUtil.setContentView(this, R.layout.activity_mypage)
 
-        binding.tvName.text = "엄준영"
-        binding.tvAge.text = "30"
+        //textview 등 component의 id는 카멜표기법으로 자동생성된다.
+        binding.tvPatientName.text = "엄준영"
+//        binding.tvAge.text = "30"
+        binding.etPatientName
 
         binding.user = User("eom", "30")
 
