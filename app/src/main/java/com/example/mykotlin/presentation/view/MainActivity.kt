d2ffa70bcb3d1d3ec7393c0753f69bd786c8a571
EomJunyoung <theoneday737@naver.com>
2021-06-22 14:19:12 +0900

    test merge branch

--- app/src/main/java/com/example/mykotlin/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/mykotlin/presentation/view/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var contactViewModel: ContactViewModel
 
+    var testbranch: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
